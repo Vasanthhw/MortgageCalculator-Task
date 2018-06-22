4e9de2dc5678f01eb029381156b9043f4cc6606f
vasantharamanv <vasantharamanv@sulekha.com>
2018-06-22 18:23:10 +0530

    Add project files.

---
+++ MortgageCalculator.Api/Services/MortgageService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MortgageCalculator.Api.Repos;
+using MortgageCalculator.Dto;
+
+namespace MortgageCalculator.Api.Services
+{
+    public interface IMortgageService
+    {
+        List<Mortgage> GetAllMortgages();
+    }
+
+    public class MortgageService : IMortgageService
+    {
+
+        private readonly IMortgageRepo _mortgageRepo;
+        public MortgageService() : this(new MortgageRepo())
+        { }
+
+        public MortgageService(IMortgageRepo mortgageRepo)
+        {
+            this._mortgageRepo = mortgageRepo;
+        }
+
+        public List<Mortgage> GetAllMortgages()
+        {
+            return _mortgageRepo.GetAllMortgages().OrderBy(x => x.MortgageType).ThenBy(x => x.InterestRate).ToList(); 
+        }
+    }
+}
---
+++ MortgageCalculator.Dto/Mortgage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MortgageCalculator.Dto
+{
+    public class Mortgage
+    {
+        private MortgageType _mortgageType;
+        private InterestRepayment _interestRepayment;
+        public int MortgageId { get; set; }
+        public string Name { get; set; }
+
+        public MortgageType MortgageType
+        {
+            get { return _mortgageType; }
+            set
+            {
+                _mortgageType = value;
+                MortgageTypeName = Enum.GetName(typeof(MortgageType), _mortgageType);
+            }
+        }
+
+        public string MortgageTypeName { get; set; }
+        public string InterestTypeName { get; set; }
+
+        public InterestRepayment InterestRepayment
+        {
+            get
+            {
+                return _interestRepayment;
+            }
+            set
+            {
+                _interestRepayment = value;
+                InterestTypeName = Enum.GetName(typeof(InterestRepayment), _interestRepayment);
+            }
+        }
+        public DateTime EffectiveStartDate { get; set; }
+        public DateTime EffectiveEndDate { get; set; }
+        public int TermsInMonths { get; set; }
+        public decimal CancellationFee { get; set; }
+        public decimal EstablishmentFee { get; set; }
+        public Guid SchemaIdentifier { get; internal set; }
+        public decimal InterestRate { get; set; }
+    }
+
+    public enum MortgageType
+    {
+        Variable,
+        Fixed
+    }
+
+    public enum InterestRepayment
+    {
+        InterestOnly,
+        PrincipalAndInterest
+    }
+}
